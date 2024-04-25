5595d449cfe17a7f9c9be6678d3c0972ba8e74b5
Nilesh Kumar Banara <nileshkumarbanara@gmail.com>
2024-04-25 17:52:14 +0530

    main

--- Sorting/selectionSort.js
+++ Sorting/selectionSort.js
@@ -1,29 +1,58 @@
-let arr = [9, 0, 1, 2, 5, 3, 7, 9, -5, -8, -10];
+// let arr = [9, 0, 1, 2, 5, 3, 7, 9, -5, -8, -10];
+
+// function selectionSort(arr) {
+//   let n = arr.length;
+//   for (let i = 0; i < n - 1; i++) {
+//     let min_Idx = findMinEle(arr, i);
+
+//     if (min_Idx !== i) {
+//       let temp = arr[i];
+//       arr[i] = arr[min_Idx];
+//       arr[min_Idx] = temp;
+//     }
+//   }
+// }
+
+// function findMinEle(arr, i) {
+//   let minIdx = i;
+
+//   for (let j = i; j < arr.length; j++) {
+//     if (arr[j] < arr[minIdx]) {
+//       minIdx = j;
+//     }
+//   }
+//   return minIdx;
+// }
+
+// selectionSort(arr);
+
+// console.log(arr);
+
+let arr = [5, 4, 3, 2, 1];
 
 function selectionSort(arr) {
   let n = arr.length;
   for (let i = 0; i < n - 1; i++) {
-    let min_Idx = findMinEle(arr, i);
-
-    if (min_Idx !== i) {
+    let min_idx = findingMinIdx(arr, i);
+    if (min_idx !== i) {
       let temp = arr[i];
-      arr[i] = arr[min_Idx];
-      arr[min_Idx] = temp;
+      arr[i] = arr[min_idx];
+      arr[min_idx] = temp;
     }
   }
+
+  return arr;
 }
 
-function findMinEle(arr, i) {
-  let minIdx = i;
-
-  for (let j = i; j < arr.length; j++) {
-    if (arr[j] < arr[minIdx]) {
-      minIdx = j;
+function findingMinIdx(arr, i) {
+  let min_idx_ele = i;
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[j] < arr[min_idx_ele]) {
+      min_idx_ele = j;
     }
   }
-  return minIdx;
+  return min_idx_ele;
 }
 
-selectionSort(arr);
-
-console.log(arr);
+let result = selectionSort(arr);
+console.log(result);
